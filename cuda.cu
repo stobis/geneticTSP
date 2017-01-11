37069b3aae83dba4864be065ea98a51d2ea91cf0
Michał Stobierski <lampard1996@gmail.com>
2017-01-12 00:52:19 +0100

    added cross function, but weak tested

--- cuda.cu
+++ cuda.cu
@@ -44,6 +44,112 @@ __device__ Point *graph;
     void cross( Chromosome *a, Chromosome *b, Chromosome *child )
     {
         //TODO
+            
+        // zalozenie: a i b to ciagi zaczynajace sie zerem, wynik zachowa te wlasnosc
+        
+        int global_thid = blockIdx.x*blockDim.x + threadIdx.x;
+        int thid = threadIdx.x;
+        
+        /* init usages */
+        
+        bool *visited = new bool[graphSize];
+        
+        for (int i = 0; i < graphSize; ++i) {;
+            visited[i] = false;
+        }
+        
+        int * res = new int[graphSize];
+        res[0] = 0; visited[0] = true;
+        int resSize = 1;
+        
+        int P = res[resSize-1]; // niezmiennik: P jest ostatnim wierzcholkiem danego, czesciowego wyniku
+        int P1 = 0, P2 = 0;
+        
+        while (resSize < graphSize) {
+            
+        
+            // P1 i P2 to miejsca gdzie jest P na sciezce w chromosomie a i b
+            
+            for (int i = 0; i < graphSize; ++i) {
+            
+                if (a->path[i] == P) {
+                    P1 = i;
+                }
+                if (b->path[i] == P) {
+                    P2 = i;
+                }
+            }
+            
+            // Szukam pierwszego wolnego wierzcholka po P na listach a i b
+            int next1 = P1 + 1, next2 = P2 + 1;
+            
+            // tam gdzie wskazuja next1 i next2 sa kandydaci do porownania (numery wierzcholkow)
+            int candidate1 = -1, candidate2 = -1;
+            
+            while (next1 < graphSize && visited[a->path[next1]]) {
+                next1++;
+            }
+            
+            if (next1 >= graphSize) {   // doszedlem do konca i nie znalazlem nieodwiedzonego w ciagu
+                // ergo musze szukac w zbiorze {1....n-1}
+                for (int i = 0; i < graphSize; ++i) {
+                    if (!visited[i]) {
+                        candidate1 = i;
+                        break;
+                    }
+                }
+            }
+            
+            while (next2 < graphSize && visited[b->path[next2]]) {
+                next2++;
+            }
+            
+            if (next2 >= graphSize) {   // doszedlem do konca i nie znalazlem nieodwiedzonego w ciagu
+                // ergo musze szukac w zbiorze {1....n-1}
+                for (int i = 0; i < graphSize; ++i) {
+                    if (!visited[i]) {
+                        candidate2 = i;
+                        break;
+                    }
+                }
+            }
+            
+            // jesli na liscie a lub b byl wolny wierzcholek. to nie szukalem dodatkowo, wiec moge przepisac wartosc
+            if (candidate1 < 0) candidate1 = a->path[next1];
+            if (candidate2 < 0) candidate2 = b->path[next2];
+            
+            // teraz musze miec odleglosci miedzy P a cand1 i P a cand2
+            
+            int dist1 = distGraph(P, candidate1);
+            int dist2 = distGraph(P, candidate2);
+            
+            // Dodaje lepsza opcje do wyniku
+            
+            if (dist1 < dist2) {
+            
+                res[resSize++] = candidate1;
+                visited[candidate1] = true;
+            }
+            else {
+            
+                res[resSize++] = candidate2;
+                visited[candidate2] = true;
+            }
+            
+            
+            // Aktualizuje niezmiennik
+            P = res[resSize-1];
+            
+        }
+        
+        // Przepisuje wynik tam gdzie trzeba
+        for (int i = 0; i < resSize; ++i) {
+            child->path[i] = res[i];
+        }
+        child->pathLength = calculatePathLength(child, child->path);
+        
+        return;
+     
     }
 
     __global__
@@ -68,6 +174,16 @@ __device__ Point *graph;
       chromosomes[thid].pathLength = calculatePathLength(chromosomes+thid, paths+thid*graphSize);
 
       printf("DUPA: %d %d\n", thid, chromosomes[thid].pathLength);
+      
+      
+      // Simple cross test for debug only!
+      __syncthreads();
+      
+      if(thid == 0) {
+      
+        cross(chromosomes, chromosomes+1, chromosomes+2);
+        printf("CHILD LEN is %d\n", chromosomes[2].pathLength);
+      }
     }
 
   __device__
@@ -83,7 +199,7 @@ __device__ Point *graph;
 
   __device__
     int dist(Point a, Point b){
-        return 1;
+        //return 1;
     	return (a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y);
     }
     
